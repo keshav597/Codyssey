f06350e0832dbf0eadc37001ca9dc601c04a5397
keshav <goyalkeshav597@gmail.com>
2026-08-24 03:38:32 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <AuthProvider> */}
+      <AuthProvider>
         <StudentProvider>
           <App />
         </StudentProvider>
-      {/* </AuthProvider> */}
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
